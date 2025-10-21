2cd4d58f54f7ba9300279e86b76c288f8597e137
Suhas B1 <suhasb1di23ai003@gmail.com>
2025-10-21 10:58:33 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,6 @@ export const firebaseConfig = {
   "projectId": "studio-8611397362-e8c68",
   "appId": "1:1038667232147:web:5264d00f71538b44adae66",
   "apiKey": "AIzaSyCOMCbZThSd_0mDu_figh1zSUvUYNfXXdw",
-  "authDomain": "studio-8611397362-e8c68.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1038667232147"
 };
